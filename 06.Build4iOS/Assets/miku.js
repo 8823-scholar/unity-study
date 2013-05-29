457d3a6a7ec0ab84303eaa6a91c58710ef16e08b
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2013-05-29 19:12:02 +0900

    study 06, build for ios.

---
+++ 06.Build4iOS/Assets/miku.js
@@ -0,0 +1,14 @@
+#pragma strict
+
+var frames : Texture2D[];
+var framesPerSecond = 10.0;
+
+function Start () {
+
+}
+
+function Update () {
+    var index : int = Time.time * framesPerSecond;
+    index = index % frames.Length;
+    guiTexture.texture = frames[index];
+}
